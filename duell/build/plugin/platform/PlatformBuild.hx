7b700c29e30c3a3facccccf6556828594257848d
kgar <khaled.garbaya@gamduell.de>
2014-09-02 18:55:10 +0200

    - flash project is running over slimerjs

--- duell/build/plugin/platform/PlatformBuild.hx
+++ duell/build/plugin/platform/PlatformBuild.hx
@@ -14,14 +14,15 @@
  import duell.objects.DuellLib;
  import duell.objects.Haxelib;
  import duell.helpers.TemplateHelper;
-
+ 
+ import sys.io.Process;
  class PlatformBuild
  {
  	public var requiredSetups = ["flash"];
 
  	private static var isDebug : Bool = false;
  	private static var isAdvancedTelemetry : Bool = false;
-
+	private var serverProcess : Process; 
  	public var targetDirectory : String;
  	public var duellBuildFlashPath : String;
  	public var projectDirectory : String;
@@ -115,13 +116,24 @@
  	}
  	public function runApp() : Void
  	{
- 		LogHelper.info("", "Launching application in "+PlatformConfiguration.getData().DEFAULT_BROWSER+" browser");
- 	    var result : Int = ProcessHelper.openURL(targetDirectory+"flash/web/index.html");
- 	    if(result != 0)
- 	    {
- 	    	LogHelper.error ("could not launch the application");
- 	    }
+ 		// LogHelper.info("", "Launching application in "+PlatformConfiguration.getData().DEFAULT_BROWSER+" browser");
+ 	 //    var result : Int = ProcessHelper.openURL(targetDirectory+"flash/web/index.html");
+ 	 //    if(result != 0)
+ 	 //    {
+ 	 //    	LogHelper.error ("could not launch the application");
+ 	 //    }
+		var result : Int = prepareAndRunHTTPServer();
+		Sys.putEnv("SLIMERJSLAUNCHER", duellBuildFlashPath+"bin/slimerjs-0.9.1/xulrunner/xulrunner");
+		ProcessHelper.runCommand(duellBuildFlashPath+"bin/slimerjs-0.9.1","python",["slimerjs.py","../test.js"]);
 
+ 	}
+ 	public function prepareAndRunHTTPServer() : Int
+ 	{
+ 		var args:Array<String> = [duellBuildFlashPath+"bin/node/http-server/http-server",targetDirectory+"flash/web","-p", "3000", "-c-1"];
+ 	    serverProcess = new Process(duellBuildFlashPath+"/bin/node/node-mac",args);
+ 	    //var result : Int = serverProcess.exitCode();
+ 	    //return result;
+ 	    return 0;
  	}
  	public function prepareFlashBuild() : Void
  	{
